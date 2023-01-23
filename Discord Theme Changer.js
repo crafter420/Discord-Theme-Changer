4f16844defe48d6af2661705a8c302cdd69bb62a
Aryan.K <aklegofan11@gmail.com>
2023-01-24 00:22:05 +0530

    Add files via upload

---
+++ Discord Theme Changer.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Discord Theme Changer
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Change the theme of Discord on a web browser
+// @author       Crafter
+// @match        https://discord.com/channels/@me
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your CSS code for the theme goes here
+    var css = `
+        /* Put your theme CSS here */
+    `;
+
+    // Create a new style element
+    var style = document.createElement("style");
+
+    // Add the CSS code to the style element
+    style.innerHTML = css;
+
+    // Append the style element to the head of the document
+    document.head.appendChild(style);
+})();
